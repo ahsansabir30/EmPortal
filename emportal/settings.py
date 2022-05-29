61e165dfef80ed0eb75b2cf4c3ff05e2346a2969
ahsansabir30 <92265482+ahsansabir30@users.noreply.github.com>
2022-05-29 10:44:45 +0100

    Update settings.py

--- emportal/settings.py
+++ emportal/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fr+qim(#&zy8dtkkl^fgtap3fltj!)zw9k0db4c--ewsnxmsko'
+SECRET_KEY = 'SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
